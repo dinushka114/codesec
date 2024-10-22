1b57a307a91814b70c49a5c102f400fae03ab7d1
rio-uml <dinushkabchandrarathna@gmail.com>
2024-10-22 23:19:49 +0530

    origin changed

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin:'https://recipemix.vercel.app/',
+    origin:'https://recipemix.vercel.app',
     credentials:true
 }));
 
